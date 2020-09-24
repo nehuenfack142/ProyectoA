f1f02aceac48ee695fd8b7619fa355fd59a1e947
Josue-Luna <josueluna650@gmail.com>
2020-09-24 12:55:21 -0300

    Login funcionando y en php

---
+++ login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>َAnimated Login Form</title>
+    <link rel="stylesheet" href="login.css">
+  </head>
+  <body>
+
+<form class="box" action="logindatabase.php" method="POST">
+  <h1>Login</h1>
+  <input type="text" name="user" placeholder="Username">
+  <input type="password" name="password" placeholder="Password">
+  <input type="submit" value="Login">
+  <span>or <a href="register.php">Register</a></span>
+</form>
+  </body>
+</html>
