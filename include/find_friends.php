72e6b186a5ce1970d0f9cd14d9b71507bfcecc91
Josue-Luna <josueluna650@gmail.com>
2020-11-06 20:02:27 -0300

    Buscar amigos incorporado

--- include/find_friends.php
+++ include/find_friends.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <?php 
 session_start();
-include ("include/find_friends_function.php");
+include("find_friends_function.php");
 
 if (!isset($_SESSION['user_email'])){
   header("location: signin.php");
@@ -9,19 +9,21 @@ if (!isset($_SESSION['user_email'])){
 else { ?>
 <html>
 <head>
-  <title>Search For Friends</title>
-  <link rel="stylesheet" type="text/css" href="https://maxcdn.booststrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <link rel="stylesheet" type="text/css" href="css/home.css">
-  <link rel="stylesheet" type="text/css" href="https://maxcdn.booststrapcdn.com/font-awesome/4.7.0/css/font-awesome.mim.css">
-
-  <script scr="https://ajax.googleapis.com/ajas/libs/jquery/3.2.1/jquery.min.js"></script>
-  </head>
+    <title>Search For Friends</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="../css/find_people.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+    <script src="https://naxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script> 
+</head>
   <body>
-    <nav class="navbar-brand" href="#">
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
       <a class="navbar-brand" href="#">
        <?php 
         $user = $_SESSION['user_email'];
-        $get_user = "select * form users where user_email='$user'";
+        $get_user = "select * from users where user_email='$user'";
         $run_user = mysqli_query($con, $get_user);
         $row  = mysqli_fetch_array($run_user);
 
@@ -30,7 +32,7 @@ else { ?>
         
         ?>
         <ul class ="navbar-nav">
-          <li><a style="color: white;text-decoration: none;font-size: 20px;"href="../account-settings.php" >Settings</a></li>
+          <li><a style="color: white;text-decoration: none;font-size: 20px;"href="../account_settings.php">Settings</a></li>
         </ul>
     </nav><br>
     <div class="row">
@@ -38,14 +40,14 @@ else { ?>
       </div>
       <div class="col-sm4">
         <form class="search_form" action="">
-          <input type="text" name="sarch_query" placeholder="Search Friends" autocomplete="off"required>
+          <input type="text" name="search_query" placeholder="Search Friends" autocomplete="off"required>
           <button class="btn" type="submit" name="search_btn">Search</button>
           
         </form>
       </div>
       <div class="col-sm--4"></div>
     </div><br><br>
-    <?php serch_user(); ?>
+    <?php search_user(); ?>
 
   </body>
   </html>
--- include/find_friends_function.php
+++ include/find_friends_function.php
@@ -1,36 +1,37 @@
 <?php
-$con = mysqli_connect("localhost", "root", "", "mychat") or die ("connection was not estrablished")
+$con = mysqli_connect("localhost", "root", "", "mychat") or die ("connection was not established");
 
 	function search_user(){
 		global $con;
 		if (isset($_GET['search_btn'])){
 			$search_query = htmlentities($_GET['search_query']);
-			$fetuser = "select * form user where user_name like '%$search_query%' or user_country"
+			$get_user = "select * from users where user_name like '%$search_query%' or user_country like '%$search_query%' ";
 		}else {
-			$get_user="SELECT * FORM USERS order by user_country, user_name DESC LIMIT 5";
+			$get_user = "SELECT * FROM users order by user_country, user_name DESC LIMIT 5";
 		}
-		$run_user = mysqli_query ($con, $ger_user); 
+		$run_user = mysqli_query($con, $get_user); 
 
-		while ( $row_user = mysqli_fetch_array($run_user)){
-			$user_name = $row_user ['user_name'];
+		while ($row_user = mysqli_fetch_array($run_user)){
+			$user_name = $row_user['user_name'];
 			$user_profile=$row_user['user_profile'];
 			$country= $row_user['user_country'];
-			$geneder = $row_user['user_gender'];
+			$gender = $row_user['user_gender'];
 			//now displaying all at once 
 
 			echo "
-			<div class='card'>
-			<img src='../$user_profile'>
-			<h1>$user_name</h1>
-			<p class = 'title'>$country</p>
-			<p>$geneder</p>
-			<form method= 'post'>
-			<p><button name = 'add'>Chat with $user_mane</button> </p>
-			</form>
-			</div><br><br>"
+				<div class='card'>
+				<img src='../$user_profile'>
+				<h1>$user_name</h1>
+				<p class = 'title'>$country</p>
+				<p>$gender</p>
+				<form method= 'post'>
+				<p><button name ='add'>Chat with $user_name</button> </p>
+				</form>
+				</div><br><br>
+				";
 		}
-		if (isset($_POST['add']){
-			echo "<script>whindow.open('../home.php?user_name=$user_name','_self)</script>"
+		if (isset($_POST['add'])){
+			echo "<script>window.open('../home.php?user_name=$user_name','_self')</script>";
 		}
 	}
 ?>
