9293cc296e67ed3cdff90a898eb563da7995077d
facucugliari <facundo.cugliari@gmail.com>
2020-11-09 14:18:54 -0300

    popup y demas

--- login.php
+++ login.php
@@ -3,19 +3,27 @@
   <head>
     <meta charset="utf-8">
     <title>َAnimated Login Form</title>
-    <link rel="stylesheet" href="login2.css" type="text/css">
+    <link rel="stylesheet" href="login1.css" type="text/css">
 
   </head>
 
   <body>
 
-    <input type="checkbox" id="cerrar">
-    <label for="cerrar" id="btn-cerrar">X</label>
-     <div class="modal"><div class="cont">
-      <h2>Holaaaa</h2>
-     </div>
+    
+    <input type="checkbox" id="btn-modal">
+    <label for="btn-modal" class="lbl-modal">Terminos y condiciones</label>
+    <div class="modal">
+      <div class="conteiner">
+      <header>     Terminos y condiciones</header>
+      <label for="btn-modal">X</label>
+      <div class="contenido">
+        <br>
+        <input type="checkbox" name="term-cond" > Acepto los <a href="JOSUUU ACA VA EL LINK">terminos y condiciones</a> expuestos aqui<br><br>
+        <input type="checkbox" name="pol-priv">Acepto la<a href="JOSUUU ACA VA EL LINK">politica de privacidad</a> que proporciona este sitio web<br>
+        <p>Para continuar presione los casilleros de arriba</p>
+      </div>
+      </div>
     </div>
-
 <main>
 <form class="box" action="logindatabase.php" method="POST">
   <h1>Login</h1>
